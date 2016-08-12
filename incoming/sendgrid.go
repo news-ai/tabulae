fd33a40c5655ffd3abffbf1362162d791da28868
Abhi Agarwal <me@abhiagarwal.com>
2016-08-12 14:41:43 -0400

    Weird event logic

--- incoming/sendgrid.go
+++ incoming/sendgrid.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-type Event struct {
+type SendGridEvent struct {
 	SgMessageID string `json:"sg_message_id"`
 	Email       string `json:"email"`
 	Timestamp   int    `json:"timestamp"`
@@ -15,14 +15,12 @@ type Event struct {
 	AsmGroupID  int    `json:"asm_group_id,omitempty"`
 }
 
-type Bounce struct {
-	Status       string   `json:"status"`
+type OpenEvent struct {
+	SendGridEvent
+
+	IP           string   `json:"ip"`
 	SgEventID    string   `json:"sg_event_id"`
-	SgMessageID  string   `json:"sg_message_id"`
-	Event        string   `json:"event"`
-	Email        string   `json:"email"`
-	Timestamp    int      `json:"timestamp"`
-	SMTPID       string   `json:"smtp-id"`
+	Useragent    string   `json:"useragent"`
 	UniqueArgKey string   `json:"unique_arg_key"`
 	Category     []string `json:"category"`
 	Newsletter   struct {
@@ -30,12 +28,60 @@ type Bounce struct {
 		NewsletterID         string `json:"newsletter_id"`
 		NewsletterSendID     string `json:"newsletter_send_id"`
 	} `json:"newsletter"`
-	AsmGroupID int    `json:"asm_group_id"`
-	Reason     string `json:"reason"`
-	Type       string `json:"type"`
-	IP         string `json:"ip"`
-	TLS        string `json:"tls"`
-	CertErr    string `json:"cert_err"`
+}
+
+type DeliveredEvent struct {
+	SendGridEvent
+
+	Response     string   `json:"response"`
+	SgEventID    string   `json:"sg_event_id"`
+	UniqueArgKey string   `json:"unique_arg_key"`
+	Category     []string `json:"category"`
+	Newsletter   struct {
+		NewsletterUserListID string `json:"newsletter_user_list_id"`
+		NewsletterID         string `json:"newsletter_id"`
+		NewsletterSendID     string `json:"newsletter_send_id"`
+	} `json:"newsletter"`
+	IP      string `json:"ip"`
+	TLS     string `json:"tls"`
+	CertErr string `json:"cert_err"`
+}
+
+type ClickEvent struct {
+	SendGridEvent
+
+	SgEventID string `json:"sg_event_id"`
+	IP        string `json:"ip"`
+	Useragent string `json:"useragent"`
+	URLOffset struct {
+		Index int    `json:"index"`
+		Type  string `json:"type"`
+	} `json:"url_offset"`
+	UniqueArgKey string   `json:"unique_arg_key"`
+	Category     []string `json:"category"`
+	Newsletter   struct {
+		NewsletterUserListID string `json:"newsletter_user_list_id"`
+		NewsletterID         string `json:"newsletter_id"`
+		NewsletterSendID     string `json:"newsletter_send_id"`
+	} `json:"newsletter"`
+}
+
+type BounceEvent struct {
+	SendGridEvent
+
+	Status       string `json:"status"`
+	SgEventID    string `json:"sg_event_id"`
+	UniqueArgKey string `json:"unique_arg_key"`
+	Newsletter   struct {
+		NewsletterUserListID string `json:"newsletter_user_list_id"`
+		NewsletterID         string `json:"newsletter_id"`
+		NewsletterSendID     string `json:"newsletter_send_id"`
+	} `json:"newsletter"`
+	Reason  string `json:"reason"`
+	Type    string `json:"type"`
+	IP      string `json:"ip"`
+	TLS     string `json:"tls"`
+	CertErr string `json:"cert_err"`
 }
 
 func SendGridHandler(w http.ResponseWriter, r *http.Request) {
