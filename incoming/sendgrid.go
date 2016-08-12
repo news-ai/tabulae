e3dedc9f38d535134ca011e0491b1ca52a0e3069
Abhi Agarwal <me@abhiagarwal.com>
2016-08-12 13:52:41 -0400

    Beginning sendgrid work

--- incoming/sendgrid.go
+++ incoming/sendgrid.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+type SendGridEvent struct {
+	SgMessageID string `json:"sg_message_id"`
+	Email       string `json:"email"`
+	Timestamp   int    `json:"timestamp"`
+	SMTPID      string `json:"smtp-id,omitempty"`
+	Event       string `json:"event"`
+	Category    string `json:"category,omitempty"`
+	URL         string `json:"url,omitempty"`
+	AsmGroupID  int    `json:"asm_group_id,omitempty"`
+}
+
 func SendGridHandler(w http.ResponseWriter, r *http.Request) {
 
 }
