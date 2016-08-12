5531cc03c8470a0a6cc1923a215d7e7f75bc65f5
Abhi Agarwal <me@abhiagarwal.com>
2016-08-12 14:15:51 -0400

    Bounce struct

--- incoming/sendgrid.go
+++ incoming/sendgrid.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-type SendGridEvent struct {
+type Event struct {
 	SgMessageID string `json:"sg_message_id"`
 	Email       string `json:"email"`
 	Timestamp   int    `json:"timestamp"`
@@ -13,6 +13,29 @@ type SendGridEvent struct {
 	Category    string `json:"category,omitempty"`
 	URL         string `json:"url,omitempty"`
 	AsmGroupID  int    `json:"asm_group_id,omitempty"`
+}
+
+type Bounce struct {
+	Status       string   `json:"status"`
+	SgEventID    string   `json:"sg_event_id"`
+	SgMessageID  string   `json:"sg_message_id"`
+	Event        string   `json:"event"`
+	Email        string   `json:"email"`
+	Timestamp    int      `json:"timestamp"`
+	SMTPID       string   `json:"smtp-id"`
+	UniqueArgKey string   `json:"unique_arg_key"`
+	Category     []string `json:"category"`
+	Newsletter   struct {
+		NewsletterUserListID string `json:"newsletter_user_list_id"`
+		NewsletterID         string `json:"newsletter_id"`
+		NewsletterSendID     string `json:"newsletter_send_id"`
+	} `json:"newsletter"`
+	AsmGroupID int    `json:"asm_group_id"`
+	Reason     string `json:"reason"`
+	Type       string `json:"type"`
+	IP         string `json:"ip"`
+	TLS        string `json:"tls"`
+	CertErr    string `json:"cert_err"`
 }
 
 func SendGridHandler(w http.ResponseWriter, r *http.Request) {
