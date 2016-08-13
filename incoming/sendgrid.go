ee5f9f62c74b535a286348e449682b3706fa394a
Abhi Agarwal <me@abhiagarwal.com>
2016-08-13 15:38:22 -0400

    SendGrid issue

--- incoming/sendgrid.go
+++ incoming/sendgrid.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
@@ -18,91 +19,7 @@ type SendGridEvent struct {
 	Email       string `json:"email"`
 	Timestamp   int    `json:"timestamp"`
 	Event       string `json:"event"`
-}
-
-type BounceEvent struct {
-	Status       string   `json:"status"`
-	SgEventID    string   `json:"sg_event_id"`
-	SgMessageID  string   `json:"sg_message_id"`
-	Event        string   `json:"event"`
-	Email        string   `json:"email"`
-	Timestamp    int      `json:"timestamp"`
-	SMTPID       string   `json:"smtp-id"`
-	UniqueArgKey string   `json:"unique_arg_key"`
-	Category     []string `json:"category"`
-	Newsletter   struct {
-		NewsletterUserListID string `json:"newsletter_user_list_id"`
-		NewsletterID         string `json:"newsletter_id"`
-		NewsletterSendID     string `json:"newsletter_send_id"`
-	} `json:"newsletter"`
-	AsmGroupID int    `json:"asm_group_id"`
-	Reason     string `json:"reason"`
-	Type       string `json:"type"`
-	IP         string `json:"ip"`
-	TLS        string `json:"tls"`
-	CertErr    string `json:"cert_err"`
-}
-
-type ClickEvent struct {
-	SgEventID   string `json:"sg_event_id"`
-	SgMessageID string `json:"sg_message_id"`
-	IP          string `json:"ip"`
-	Useragent   string `json:"useragent"`
-	Event       string `json:"event"`
-	Email       string `json:"email"`
-	Timestamp   int    `json:"timestamp"`
-	URL         string `json:"url"`
-	URLOffset   struct {
-		Index int    `json:"index"`
-		Type  string `json:"type"`
-	} `json:"url_offset"`
-	UniqueArgKey string   `json:"unique_arg_key"`
-	Category     []string `json:"category"`
-	Newsletter   struct {
-		NewsletterUserListID string `json:"newsletter_user_list_id"`
-		NewsletterID         string `json:"newsletter_id"`
-		NewsletterSendID     string `json:"newsletter_send_id"`
-	} `json:"newsletter"`
-	AsmGroupID int `json:"asm_group_id"`
-}
-
-type DeliveredEvent struct {
-	Response     string   `json:"response"`
-	SgEventID    string   `json:"sg_event_id"`
-	SgMessageID  string   `json:"sg_message_id"`
-	Event        string   `json:"event"`
-	Email        string   `json:"email"`
-	Timestamp    int      `json:"timestamp"`
-	SMTPID       string   `json:"smtp-id"`
-	UniqueArgKey string   `json:"unique_arg_key"`
-	Category     []string `json:"category"`
-	Newsletter   struct {
-		NewsletterUserListID string `json:"newsletter_user_list_id"`
-		NewsletterID         string `json:"newsletter_id"`
-		NewsletterSendID     string `json:"newsletter_send_id"`
-	} `json:"newsletter"`
-	AsmGroupID int    `json:"asm_group_id"`
-	IP         string `json:"ip"`
-	TLS        string `json:"tls"`
-	CertErr    string `json:"cert_err"`
-}
-
-type OpenEvent struct {
-	Email        string   `json:"email"`
-	Timestamp    int      `json:"timestamp"`
-	IP           string   `json:"ip"`
-	SgEventID    string   `json:"sg_event_id"`
-	SgMessageID  string   `json:"sg_message_id"`
-	Useragent    string   `json:"useragent"`
-	Event        string   `json:"event"`
-	UniqueArgKey string   `json:"unique_arg_key"`
-	Category     []string `json:"category"`
-	Newsletter   struct {
-		NewsletterUserListID string `json:"newsletter_user_list_id"`
-		NewsletterID         string `json:"newsletter_id"`
-		NewsletterSendID     string `json:"newsletter_send_id"`
-	} `json:"newsletter"`
-	AsmGroupID int `json:"asm_group_id"`
+	Reason      string `json:"reason"`
 }
 
 func SendGridHandler(w http.ResponseWriter, r *http.Request) {
@@ -128,57 +45,30 @@ func SendGridHandler(w http.ResponseWriter, r *http.Request) {
 			singleEvent := allEvents[i]
 
 			// Validate email exists with particular SendGridId
-			email, err := controllers.FilterEmailBySendGridID(c, singleEvent.SgMessageID)
+			sendGridId := strings.Split(singleEvent.SgMessageID, ".")[0]
+			email, err := controllers.FilterEmailBySendGridID(c, sendGridId)
 			if err != nil {
-				log.Errorf(c, "%v", err)
+				log.Errorf(c, "%v with value %v", err, sendGridId)
 			}
-
-			// Another decoder variable
-			rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
-			eventDecoder := json.NewDecoder(rdr2)
 
 			// Add to appropriate Email model
 			switch singleEvent.Event {
 			case "bounce":
-				var bounceEvent BounceEvent
-				err = eventDecoder.Decode(&bounceEvent)
-				if err != nil {
-					log.Errorf(c, "%v", err)
-				}
-
-				_, err = email.MarkBounced(c, bounceEvent.Reason)
+				_, err = email.MarkBounced(c, singleEvent.Reason)
 				if err != nil {
 					log.Errorf(c, "%v", err)
 				}
 			case "click":
-				var clickEvent ClickEvent
-				err := eventDecoder.Decode(&clickEvent)
-				if err != nil {
-					log.Errorf(c, "%v", err)
-				}
-
 				_, err = email.MarkClicked(c)
 				if err != nil {
 					log.Errorf(c, "%v", err)
 				}
 			case "delivered":
-				var delieveredEvent DeliveredEvent
-				err := eventDecoder.Decode(&delieveredEvent)
-				if err != nil {
-					log.Errorf(c, "%v", err)
-				}
-
 				_, err = email.MarkDelivered(c)
 				if err != nil {
 					log.Errorf(c, "%v", err)
 				}
 			case "open":
-				var openEvent OpenEvent
-				err := eventDecoder.Decode(&openEvent)
-				if err != nil {
-					log.Errorf(c, "%v", err)
-				}
-
 				_, err = email.MarkOpened(c)
 				if err != nil {
 					log.Errorf(c, "%v", err)
