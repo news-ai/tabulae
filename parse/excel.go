6ba792ae0a8d160fbae4518987cd9908e60b4704
Abhi Agarwal <me@abhiagarwal.com>
2016-08-14 00:02:25 -0400

    Initial attempt at parsing a new header file

--- parse/excel.go
+++ parse/excel.go
@@ -8,8 +8,12 @@ import (
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 
+	"github.com/news-ai/tabulae/models"
+
 	"github.com/tealeg/xlsx"
 )
+
+var nonCustomHeaders = [...]string{"firstname", "lastname", "email", "employers", "pastemployers", "notes", "linkedin", "twitter", "instagram", "website", "blog"}
 
 type Column struct {
 	Rows []string `json:"rows"`
@@ -55,4 +59,43 @@ func FileToExcelHeader(r *http.Request, file []byte) ([]Column, error) {
 	}
 
 	return columns, nil
+}
+
+func rowToContactStruct(r *http.Request, c context.Context, singleRow xlsx.Row, headers []string) (models.Contact, error) {
+	var contact models.Contact
+
+	return contact, nil
+}
+
+func ExcelHeadersToListModel(r *http.Request, file []byte, headers []string) (models.MediaList, error) {
+	c := appengine.NewContext(r)
+
+	customHeaders := []string{}
+
+	xlFile, err := xlsx.OpenBinary(file)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return models.MediaList{}, err
+	}
+
+	if len(xlFile.Sheets) == 0 {
+		err = errors.New("Sheet is empty")
+		log.Errorf(c, "%v", err)
+		return models.MediaList{}, err
+	}
+
+	sheet := xlFile.Sheets[0]
+
+	if len(sheet.Rows) == 0 {
+		err = errors.New("No rows in sheet")
+		log.Errorf(c, "%v", err)
+		return models.MediaList{}, err
+	}
+
+	var list models.MediaList
+
+	// Loop through all the rows
+	// Extract information
+
+	return list, nil
 }
