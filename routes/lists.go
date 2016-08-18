6f90de23b8153bb400485bf4265b3b94339d550e
Abhi Agarwal <me@abhiagarwal.com>
2016-08-18 17:31:05 -0400

    Moving files into a action handler

--- routes/lists.go
+++ routes/lists.go
@@ -4,16 +4,70 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"golang.org/x/net/context"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 
 	"github.com/gorilla/mux"
 
 	"github.com/news-ai/tabulae/controllers"
+	"github.com/news-ai/tabulae/files"
 	"github.com/news-ai/tabulae/permissions"
+	"github.com/news-ai/tabulae/utils"
 )
+
+var (
+	errMediaListHandling = "Media List handling error"
+)
+
+func handleMediaListActionUpload(c context.Context, r *http.Request, id string, action string, limit int, offset int) (interface{}, error) {
+
+	user, err := GetUser(r)
+	if err != nil {
+		return nil, err
+	}
+	userId := strconv.FormatInt(user.Id, 10)
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return nil, err
+	}
+
+	noSpaceFileName := ""
+	if handler.Filename != "" {
+		noSpaceFileName = strings.Replace(handler.Filename, " ", "", -1)
+	}
+
+	fileName := strings.Join([]string{userId, id, utils.RandToken(), noSpaceFileName}, "-")
+	val, err := files.UploadFile(r, fileName, file, userId, id, handler.Header.Get("Content-Type"))
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return nil, err
+	}
+
+	return val, nil
+}
+
+func handleMediaListActions(c context.Context, r *http.Request, id string, action string, limit int, offset int) (interface{}, error) {
+	switch r.Method {
+	case "GET":
+		switch action {
+		case "contacts":
+			return controllers.GetContactsForList(c, r, id, limit, offset)
+		}
+	case "POST":
+		switch action {
+		case "upload":
+			return controllers.GetContactsForList(c, r, id, limit, offset)
+		}
+	}
+	return nil, errors.New("method not implemented")
+}
 
 func handleMediaList(c context.Context, r *http.Request, id string) (interface{}, error) {
 	switch r.Method {
@@ -47,7 +101,7 @@ func MediaListsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err != nil {
-		permissions.ReturnError(w, http.StatusInternalServerError, "Media Lists handling error", err.Error())
+		permissions.ReturnError(w, http.StatusInternalServerError, errMediaListHandling, err.Error())
 		return
 	}
 }
@@ -68,7 +122,37 @@ func MediaListHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err != nil {
-			permissions.ReturnError(w, http.StatusInternalServerError, "Media List handling error", err.Error())
+			permissions.ReturnError(w, http.StatusInternalServerError, errMediaListHandling, err.Error())
+			return
+		}
+	}
+}
+
+// Handler for when the user wants to perform an action on the lists
+func MediaListActionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	c := appengine.NewContext(r)
+
+	// Get Id and Action
+	vars := mux.Vars(r)
+	id, idOk := vars["id"]
+	action, actionOk := vars["action"]
+
+	if idOk && actionOk {
+		limit, offset, err := GetPagination(r)
+		if err != nil {
+			permissions.ReturnError(w, http.StatusInternalServerError, errMediaListHandling, err.Error())
+			return
+		}
+
+		val, err := handleMediaListActions(c, r, id, action, limit, offset)
+
+		if err == nil {
+			err = json.NewEncoder(w).Encode(val)
+		}
+
+		if err != nil {
+			permissions.ReturnError(w, http.StatusInternalServerError, errMediaListHandling, err.Error())
 			return
 		}
 	}
