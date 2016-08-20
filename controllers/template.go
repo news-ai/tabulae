01446a8c1235c6dca58e7c6bd3717a877a489baa
Abhi Agarwal <me@abhiagarwal.com>
2016-08-20 12:30:17 -0400

    Getting all templates

--- controllers/template.go
+++ controllers/template.go
@@ -41,3 +41,46 @@ func getTemplate(c context.Context, id int64) (models.Template, error) {
 	}
 	return models.Template{}, errors.New("No template by this id")
 }
+
+/*
+* Public methods
+ */
+
+/*
+* Get methods
+ */
+
+// Gets every template
+func GetTemplates(c context.Context, r *http.Request) ([]models.Template, error) {
+	user, err := GetCurrentUser(c, r)
+	if err != nil {
+		return []models.Template{}, err
+	}
+
+	if !user.IsAdmin {
+		return []models.Template{}, errors.New("Forbidden")
+	}
+
+	offset := gcontext.Get(r, "offset").(int)
+	limit := gcontext.Get(r, "limit").(int)
+
+	ks, err := datastore.NewQuery("Template").Filter("CreatedBy =", user.Id).Limit(limit).Offset(offset).KeysOnly().GetAll(c, nil)
+	if err != nil {
+		return []models.Template{}, err
+	}
+
+	var templates []models.Template
+	templates = make([]models.Template, len(ks))
+
+	err = nds.GetMulti(c, ks, templates)
+	if err != nil {
+		log.Infof(c, "%v", err)
+		return []models.Template{}, err
+	}
+
+	for i := 0; i < len(templates); i++ {
+		templates[i].Id = ks[i].IntID()
+	}
+
+	return templates, nil
+}
