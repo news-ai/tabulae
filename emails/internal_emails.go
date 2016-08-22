1bfcb754d83b15a68b4c396b5c86c60bce4eafdd
Abhi Agarwal <me@abhiagarwal.com>
2016-08-21 23:36:47 -0400

    Deploying confirmation email for sendgrid id

--- emails/internal_emails.go
+++ emails/internal_emails.go
@@ -13,6 +13,38 @@ import (
 )
 
 // Send an email confirmation to a new user
+func SendConfirmationEmailNew(r *http.Request, email models.Email, user models.User) (bool, string, error) {
+	c := appengine.NewContext(r)
+
+	sendgrid.DefaultClient.HTTPClient = urlfetch.Client(c)
+
+	userFullName := strings.Join([]string{user.FirstName, user.LastName}, " ")
+	emailFullName := strings.Join([]string{email.FirstName, email.LastName}, " ")
+
+	from := mail.NewEmail(userFullName, user.Email)
+	to := mail.NewEmail(emailFullName, email.To)
+	content := mail.NewContent("text/html", email.Body)
+	m := mail.NewV3MailInit(from, email.Subject, to, content)
+
+	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
+	request.Method = "POST"
+	request.Body = mail.GetRequestBody(m)
+
+	// Send the actual mail here
+	response, err := sendgrid.API(request)
+	if err != nil {
+		log.Errorf(c, "error: %v", err)
+		return false, "", err
+	}
+
+	emailId := ""
+	if len(response.Headers["X-Message-Id"]) > 0 {
+		emailId = response.Headers["X-Message-Id"][0]
+	}
+	return true, emailId, nil
+}
+
+// Send an email confirmation to a new user
 func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode string) {
 	c := appengine.NewContext(r)
 	sg.Client = urlfetch.Client(c)
