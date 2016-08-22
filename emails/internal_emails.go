af4033fc0c1b1bc6f60e288f132f2f9c61c6df75
Abhi Agarwal <me@abhiagarwal.com>
2016-08-22 01:04:30 -0400

    Test switching to sendgrid v3

--- emails/internal_emails.go
+++ emails/internal_emails.go
@@ -3,28 +3,40 @@ package emails
 import (
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
 
 	"github.com/news-ai/tabulae/models"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 
-	"gopkg.in/sendgrid/sendgrid-go.v2"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 // Send an email confirmation to a new user
-func SendConfirmationEmailNew(r *http.Request, email models.Email, user models.User) (bool, string, error) {
+func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode string) (bool, string, error) {
 	c := appengine.NewContext(r)
 
 	sendgrid.DefaultClient.HTTPClient = urlfetch.Client(c)
 
-	userFullName := strings.Join([]string{user.FirstName, user.LastName}, " ")
 	emailFullName := strings.Join([]string{email.FirstName, email.LastName}, " ")
 
-	from := mail.NewEmail(userFullName, user.Email)
+	from := mail.NewEmail("Abhi from NewsAI", "abhi@newsai.org")
 	to := mail.NewEmail(emailFullName, email.To)
 	content := mail.NewContent("text/html", email.Body)
-	m := mail.NewV3MailInit(from, email.Subject, to, content)
+	m := mail.NewV3MailInit(from, "Thanks for signing up!", to, content)
+	m.SetTemplateID("a64e454c-19d5-4bba-9cef-bd185e7c9b0b")
+
+	// personalization := mail.Personalization{}
+	// personalization.Substitutions
+
+	// m.AddPersonalizations()
+	// m.AddS
+
+	//url.QueryEscape(confirmationCode)
 
 	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
 	request.Method = "POST"
@@ -44,22 +56,22 @@ func SendConfirmationEmailNew(r *http.Request, email models.Email, user models.U
 	return true, emailId, nil
 }
 
-// Send an email confirmation to a new user
-func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode string) {
-	c := appengine.NewContext(r)
-	sg.Client = urlfetch.Client(c)
+// // Send an email confirmation to a new user
+// func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode string) {
+// 	c := appengine.NewContext(r)
+// 	sg.Client = urlfetch.Client(c)
 
-	m := sendgrid.NewMail()
-	m.AddTo(email.To)
-	m.AddToName(email.FirstName + " " + email.LastName)
-	m.SetSubject("Thanks for signing up!")
-	m.SetHTML(" ")
-	m.SetText(" ")
-	m.SetFrom("Abhi from NewsAI <abhi@newsai.org>")
-	m.SetReplyTo(fromNewsAIEmail.Name)
-	m.SetReplyToEmail(&fromNewsAIEmail)
-	m.AddFilter("templates", "enable", "1")
-	m.AddFilter("templates", "template_id", "a64e454c-19d5-4bba-9cef-bd185e7c9b0b")
-	m.AddSubstitution("{CONFIRMATION_CODE}", url.QueryEscape(confirmationCode))
-	sg.Send(m)
-}
+// 	m := sendgrid.NewMail()
+// 	m.AddTo(email.To)
+// 	m.AddToName(email.FirstName + " " + email.LastName)
+// 	m.SetSubject("Thanks for signing up!")
+// 	m.SetHTML(" ")
+// 	m.SetText(" ")
+// 	m.SetFrom("Abhi from NewsAI <abhi@newsai.org>")
+// 	m.SetReplyTo(fromNewsAIEmail.Name)
+// 	m.SetReplyToEmail(&fromNewsAIEmail)
+// 	m.AddFilter("templates", "enable", "1")
+// 	m.AddFilter("templates", "template_id", "a64e454c-19d5-4bba-9cef-bd185e7c9b0b")
+// 	m.AddSubstitution("{CONFIRMATION_CODE}", url.QueryEscape(confirmationCode))
+// 	sg.Send(m)
+// }
