3a3f5a60b8cc490f5193cbf26f47d285076165fd
Abhi Agarwal <me@abhiagarwal.com>
2016-08-22 11:58:42 -0400

    Testing sending internal confirmation emails in production

--- emails/internal_emails.go
+++ emails/internal_emails.go
@@ -49,29 +49,11 @@ func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode
 		return false, "", err
 	}
 
+	// 	m.AddSubstitution("{CONFIRMATION_CODE}", url.QueryEscape(confirmationCode))
+
 	emailId := ""
 	if len(response.Headers["X-Message-Id"]) > 0 {
 		emailId = response.Headers["X-Message-Id"][0]
 	}
 	return true, emailId, nil
 }
-
-// // Send an email confirmation to a new user
-// func SendConfirmationEmail(r *http.Request, email models.Email, confirmationCode string) {
-// 	c := appengine.NewContext(r)
-// 	sg.Client = urlfetch.Client(c)
-
-// 	m := sendgrid.NewMail()
-// 	m.AddTo(email.To)
-// 	m.AddToName(email.FirstName + " " + email.LastName)
-// 	m.SetSubject("Thanks for signing up!")
-// 	m.SetHTML(" ")
-// 	m.SetText(" ")
-// 	m.SetFrom("Abhi from NewsAI <abhi@newsai.org>")
-// 	m.SetReplyTo(fromNewsAIEmail.Name)
-// 	m.SetReplyToEmail(&fromNewsAIEmail)
-// 	m.AddFilter("templates", "enable", "1")
-// 	m.AddFilter("templates", "template_id", "a64e454c-19d5-4bba-9cef-bd185e7c9b0b")
-// 	m.AddSubstitution("{CONFIRMATION_CODE}", url.QueryEscape(confirmationCode))
-// 	sg.Send(m)
-// }
