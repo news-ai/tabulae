b6d6078826b3c62e8dbd00f7a2568c50524a1139
Abhi Agarwal <me@abhiagarwal.com>
2016-08-23 18:23:37 -0400

    Closes #50: list has default fields

--- utils/path.go
+++ utils/path.go
@@ -7,13 +7,13 @@ import (
 var APIURL = ""
 
 func InitURL() {
-	if os.Getenv("BASE_URL") != "" {
-		APIURL = os.Getenv("BASE_URL")
+	if os.Getenv("RUN_WITH_DEVAPPSERVER") == "1" {
+		APIURL = "http://localhost:8080/api"
 		return
 	}
 
-	if os.Getenv("RUN_WITH_DEVAPPSERVER") == "1" {
-		APIURL = "http://localhost:8080/api"
+	if os.Getenv("BASE_URL") != "" {
+		APIURL = os.Getenv("BASE_URL")
 	} else {
 		APIURL = "https://tabulae.newsai.org/api"
 	}
