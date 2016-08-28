40b39e548eec10686a6d29aafdd179560d14f727
Abhi Agarwal <me@abhiagarwal.com>
2016-08-28 14:08:11 -0400

    Adding ES search query

--- search/agencies.go
+++ search/agencies.go
@@ -1,30 +1,61 @@
 package search
 
 import (
+	"errors"
+
 	"golang.org/x/net/context"
 
 	"google.golang.org/appengine/log"
 
-	"gopkg.in/olivere/elastic.v3"
+	"github.com/news-ai/tabulae/models"
 )
 
-type AgencySearch struct {
-	Id   int64
-	Name string
+type AgencyResponse struct {
+	Took     int  `json:"took"`
+	TimedOut bool `json:"timed_out"`
+	Shards   struct {
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Failed     int `json:"failed"`
+	} `json:"_shards"`
+	Hits struct {
+		Total    int     `json:"total"`
+		MaxScore float64 `json:"max_score"`
+		Hits     []struct {
+			Index  string  `json:"_index"`
+			Type   string  `json:"_type"`
+			ID     string  `json:"_id"`
+			Score  float64 `json:"_score"`
+			Source struct {
+				Data models.Agency `json:"data"`
+			} `json:"_source"`
+		} `json:"hits"`
+	} `json:"hits"`
 }
 
 func SearchAgency(c context.Context, search string) (interface{}, error) {
-	termQuery := elastic.NewTermQuery("data.Name", search)
-	searchResult, err := elasticClient.Search().Index("Agency").Query(termQuery).Do()
-
-	log.Infof(c, "%v", searchResult)
-
+	client := urlfetch.Client(c)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=" + tkn.AccessToken)
 	if err != nil {
-		log.Errorf(c, "%v", err)
 		return nil, err
 	}
 
-	log.Infof(c, "%v", searchResult)
+	decoder := json.NewDecoder(resp.Body)
+	var agencyResponse AgencyResponse
+	err = decoder.Decode(&agencyResponse)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	agencies := []models.Agency{}
+	if agencyResponse.Hits.Total == 0 {
+		return agencies, nil
+	}
+
+	agencyHits := agencyResponse.Hits.Hits
+	for i := 0; i < len(agencyHits); i++ {
+		agencyHits = append(agencyHits, agencyHits[i].Source.Data)
+	}
+
+	return agencyHits, nil
 }
