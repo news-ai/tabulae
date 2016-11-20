cb3d4fd0d4a88dc6a194b9d6385cd1ffa83a4a02
Abhi Agarwal <me@abhiagarwal.com>
2016-11-19 20:22:57 -0500

    finishing attachment

--- attach/attach.go
+++ attach/attach.go
@@ -11,44 +11,46 @@ import (
 	"github.com/news-ai/tabulae/models"
 )
 
-func ReadAttachment(r *http.Request, file models.File) ([]byte, string, error) {
+func ReadAttachment(r *http.Request, file models.File) ([]byte, string, string, error) {
 	c := appengine.NewContext(r)
 
 	client, err := storage.NewClient(c)
 	if err != nil {
-		return nil, "", err
+		return nil, "", "", err
 	}
 	defer client.Close()
 
 	clientBucket := client.Bucket("tabulae-email-attachment")
 	rc, err := clientBucket.Object(file.FileName).NewReader(c)
 	if err != nil {
-		return nil, "", err
+		return nil, "", "", err
 	}
 	defer rc.Close()
 
 	data, err := ioutil.ReadAll(rc)
 	if err != nil {
-		return nil, "", err
+		return nil, "", "", err
 	}
 
-	return data, rc.ContentType(), nil
+	return data, rc.ContentType(), file.FileName, nil
 }
 
-func GetAttachmentsForEmail(r *http.Request, email models.Email, files []models.File) ([][]byte, []string, error) {
+func GetAttachmentsForEmail(r *http.Request, email models.Email, files []models.File) ([][]byte, []string, []string, error) {
 	c := appengine.NewContext(r)
 
 	bytesArray := [][]byte{}
 	attachmentTypes := []string{}
+	fileNames := []string{}
 	for i := 0; i < len(files); i++ {
-		currentBytes, attachmentType, err := ReadAttachment(r, files[i])
+		currentBytes, attachmentType, fileName, err := ReadAttachment(r, files[i])
 		if err == nil {
 			bytesArray = append(bytesArray, currentBytes)
 			attachmentTypes = append(attachmentTypes, attachmentType)
+			fileNames = append(fileNames, fileName)
 		} else {
 			log.Errorf(c, "%v", err)
 		}
 	}
 
-	return bytesArray, attachmentTypes
+	return bytesArray, attachmentTypes, fileNames, nil
 }
