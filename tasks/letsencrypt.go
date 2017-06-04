ed7cc4c194d025a6532dcdbee08d888bf9b0617c
Abhi Agarwal <me@abhiagarwal.com>
2017-06-04 16:54:54 -0400

    Moving files over to API

--- tasks/letsencrypt.go
+++ tasks/letsencrypt.go
@@ -7,6 +7,6 @@ import (
 
 func LetsEncryptValidation(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain")
-	fmt.Fprintf(w, "cK4fsCUnbTkr0j5fkuYM-V9NwoET2JP_4iRTBBvLDUE.oKzw5QYkN1q8zhhAe-jdS_VxeEiIqz4MC7pSvnuwGq4")
+	fmt.Fprintf(w, "ZCLfT3oIOdBK0iUF28viK2IEvmjJ46_8NzBEE0F6jxA.oKzw5QYkN1q8zhhAe-jdS_VxeEiIqz4MC7pSvnuwGq4")
 	return
 }
