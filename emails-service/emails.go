9fcd4e2f864acdecca3df91c45c61552938e464b
Abhi Agarwal <me@abhiagarwal.com>
2017-08-22 16:58:02 -0400

    working pubsub

--- emails-service/emails.go
+++ emails-service/emails.go
@@ -5,26 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	// "strconv"
 
 	"golang.org/x/net/context"
 
+	"cloud.google.com/go/datastore"
 	"cloud.google.com/go/pubsub"
+
+	tabulaeModels "github.com/news-ai/tabulae/models"
 )
 
 var (
-	PubsubClient *pubsub.Client
-	subscription *pubsub.Subscription
+	pubsubClient    *pubsub.Client
+	subscription    *pubsub.Subscription
+	datastoreClient *datastore.Client
 )
+
+func getEmails(c context.Context, ids []int64) ([]tabulaeModels.Email, error) {
+	var keys []*datastore.Key
+	for i := 0; i < len(ids); i++ {
+		emailId := datastore.IDKey("Email", ids[i], nil)
+		keys = append(keys, emailId)
+	}
+
+	emails := make([]tabulaeModels.Email, len(keys))
+	if err := datastoreClient.GetMulti(c, keys, emails); err != nil {
+		log.Printf("%v", err)
+		return []tabulaeModels.Email{}, err
+	}
+
+	return emails, nil
+}
 
 func subscribe() {
 	c := context.Background()
 	err := subscription.Receive(c, func(c context.Context, msg *pubsub.Message) {
 		var ids []int64
 		if err := json.Unmarshal(msg.Data, &ids); err != nil {
+			log.Printf("%v", err)
 			log.Printf("could not decode message data: %#v", msg)
 			msg.Ack()
 			return
 		}
+
+		emails, err := getEmails(c, ids)
+		if err != nil {
+			log.Printf("%v", err)
+			msg.Ack()
+			return
+		}
+
+		log.Printf("%v", emails)
+
+		msg.Ack()
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -32,11 +65,22 @@ func subscribe() {
 }
 
 func main() {
+	projectID := "newsai-1166"
 	c := context.Background()
 
-	PubsubClient, err := pubsub.NewClient(c, "newsai-1166")
-	topic := PubsubClient.Topic("tabulae-emails-service")
-	subscription, _ = PubsubClient.CreateSubscription(c, "appengine-flex-service-1", pubsub.SubscriptionConfig{Topic: topic})
+	pubsubClient, err := pubsub.NewClient(c, projectID)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	topic := pubsubClient.Topic("tabulae-emails-service")
+	subscription, _ = pubsubClient.CreateSubscription(c, "appengine-flex-service-1", pubsub.SubscriptionConfig{Topic: topic})
+
+	datastoreClient, err = datastore.NewClient(c, projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
 	go subscribe()
 
 	http.HandleFunc("/", handle)
