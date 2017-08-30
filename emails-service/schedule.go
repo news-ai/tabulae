9cc7087e60437e70b67a072baafced80be1c25d8
Abhi Agarwal <me@abhiagarwal.com>
2017-08-29 21:00:53 -0400

    moving sch to es

--- emails-service/schedule.go
+++ emails-service/schedule.go
@@ -1,276 +1,311 @@
 package main
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"net/http"
-// 	"strconv"
-// 	"time"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
 
-// 	"golang.org/x/net/context"
+	"golang.org/x/net/context"
 
-// 	"google.golang.org/appengine"
-// 	"google.golang.org/appengine/urlfetch"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 
-// 	apiControllers "github.com/news-ai/api/controllers"
+	apiControllers "github.com/news-ai/api/controllers"
 
-// 	"github.com/news-ai/tabulae/controllers"
-// 	"google.golang.org/appengine/log"
+	"github.com/news-ai/tabulae/controllers"
+	"google.golang.org/appengine/log"
 
-// 	"github.com/news-ai/tabulae/models"
-// 	"github.com/news-ai/tabulae/sync"
+	"github.com/news-ai/tabulae/models"
+	"github.com/news-ai/tabulae/sync"
 
-// 	"github.com/news-ai/web/emails"
-// 	"github.com/news-ai/web/google"
-// 	"github.com/news-ai/web/outlook"
-// )
+	"github.com/news-ai/web/emails"
+	"github.com/news-ai/web/google"
+	"github.com/news-ai/web/outlook"
+)
 
-// // When the email is "Delievered == false" and has a "SendAt" date
-// // And "Cancel == false"
+// When the email is "Delievered == false" and has a "SendAt" date
+// And "Cancel == false"
 
-// func SchedueleEmailTask(w http.ResponseWriter, r *http.Request) {
-// 	c := appengine.NewContext(r)
-// 	hasErrors := false
-// 	schedueled, err := controllers.GetCurrentSchedueledEmails(c, r)
-// 	if err != nil {
-// 		log.Errorf(c, "%v", err)
-// 		w.WriteHeader(500)
-// 		return
-// 	}
+func GetCurrentSchedueledEmails(c context.Context, r *http.Request) ([]models.Email, error) {
+	emails := []models.Email{}
 
-// 	log.Infof(c, "%v", len(schedueled))
+	timeNow := time.Now()
+	currentTime := time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), timeNow.Hour(), timeNow.Minute(), 0, 0, time.FixedZone("GMT", 0))
 
-// 	// Loop through the emails and send them
-// 	emailIds := []int64{}
-// 	for i := 0; i < len(schedueled); i++ {
-// 		user, err := apiControllers.GetUserByIdUnauthorized(c, r, schedueled[i].CreatedBy)
-// 		if err != nil {
-// 			hasErrors = true
-// 			log.Errorf(c, "%v", err)
-// 			continue
-// 		}
+	// When the email is "Delievered == false" and has a "SendAt" date
+	// And "Cancel == false". Also a filer if the user has sent it already or not
+	query := datastore.NewQuery("Email").Filter("SendAt <=", currentTime).Filter("IsSent =", true).Filter("Delievered =", false).Filter("Cancel =", false)
 
-// 		files := []models.File{}
-// 		if len(schedueled[i].Attachments) > 0 {
-// 			for x := 0; x < len(schedueled[i].Attachments); x++ {
-// 				file, _, err := controllers.GetFileByIdUnauthorized(c, r, schedueled[i].Attachments[x])
-// 				if err == nil {
-// 					files = append(files, file)
-// 				} else {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 				}
-// 			}
-// 		}
+	ks, err := query.KeysOnly().GetAll(c, nil)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return []models.Email{}, err
+	}
 
-// 		if schedueled[i].Method == "gmail" {
-// 			if user.AccessToken != "" && user.Gmail {
-// 				err = google.ValidateAccessToken(r, user)
-// 				// Refresh access token if err is nil
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					user, err = google.RefreshAccessToken(r, user)
-// 					if err != nil {
-// 						hasErrors = true
-// 						log.Errorf(c, "%v", err)
-// 						continue
-// 					}
-// 				}
+	emails = make([]models.Email, len(ks))
+	err = nds.GetMulti(c, ks, emails)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return []models.Email{}, err
+	}
 
-// 				log.Infof(c, "%v", files)
-// 				log.Infof(c, "%v", schedueled[i])
-// 				gmailId, gmailThreadId, err := emails.SendGmailEmail(r, user, schedueled[i], files)
-// 				if err != nil {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 					continue
-// 				}
+	emailsToSend := []models.Email{}
+	for i := 0; i < len(emails); i++ {
+		emails[i].Format(ks[i], "emails")
 
-// 				schedueled[i].GmailId = gmailId
-// 				schedueled[i].GmailThreadId = gmailThreadId
+		if !emails[i].SendAt.IsZero() {
+			emailsToSend = append(emailsToSend, emails[i])
+		}
+	}
 
-// 				_, err = schedueled[i].MarkDelivered(c)
-// 				if err != nil {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 					continue
-// 				}
+	return emailsToSend, nil
+}
 
-// 				// if files are present
-// 				for i := 0; i < len(files); i++ {
-// 					files[i].Imported = true
-// 					files[i].Save(c)
-// 				}
+func SchedueleEmailTask(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	hasErrors := false
+	schedueled, err := GetCurrentSchedueledEmails(c, r)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		w.WriteHeader(500)
+		return
+	}
 
-// 				// Add to emailids array
-// 				emailIds = append(emailIds, schedueled[i].Id)
-// 			}
-// 		} else if schedueled[i].Method == "outlook" {
-// 			if user.OutlookAccessToken != "" && user.Outlook {
-// 				err = outlook.ValidateAccessToken(r, user)
-// 				// Refresh access token if err is nil
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					user, err = outlook.RefreshAccessToken(r, user)
-// 					if err != nil {
-// 						hasErrors = true
-// 						log.Errorf(c, "%v", err)
-// 						continue
-// 					}
-// 				}
+	log.Infof(c, "%v", len(schedueled))
 
-// 				log.Infof(c, "%v", files)
-// 				log.Infof(c, "%v", schedueled[i])
-// 				err := emails.SendOutlookEmail(r, user, schedueled[i], files)
-// 				if err != nil {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 					continue
-// 				}
+	// Loop through the emails and send them
+	emailIds := []int64{}
+	for i := 0; i < len(schedueled); i++ {
+		user, err := apiControllers.GetUserByIdUnauthorized(c, r, schedueled[i].CreatedBy)
+		if err != nil {
+			hasErrors = true
+			log.Errorf(c, "%v", err)
+			continue
+		}
 
-// 				_, err = schedueled[i].MarkDelivered(c)
-// 				// sync.ResourceSync(r, schedueled[i].Id, "Email", "create")
-// 				if err != nil {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 					continue
-// 				}
+		files := []models.File{}
+		if len(schedueled[i].Attachments) > 0 {
+			for x := 0; x < len(schedueled[i].Attachments); x++ {
+				file, _, err := controllers.GetFileByIdUnauthorized(c, r, schedueled[i].Attachments[x])
+				if err == nil {
+					files = append(files, file)
+				} else {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+				}
+			}
+		}
 
-// 				// if files are present
-// 				for i := 0; i < len(files); i++ {
-// 					files[i].Imported = true
-// 					files[i].Save(c)
-// 				}
+		if schedueled[i].Method == "gmail" {
+			if user.AccessToken != "" && user.Gmail {
+				err = google.ValidateAccessToken(r, user)
+				// Refresh access token if err is nil
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					user, err = google.RefreshAccessToken(r, user)
+					if err != nil {
+						hasErrors = true
+						log.Errorf(c, "%v", err)
+						continue
+					}
+				}
 
-// 				// Add to emailids array
-// 				emailIds = append(emailIds, schedueled[i].Id)
-// 			}
-// 		} else if schedueled[i].Method == "smtp" {
-// 			// If scheduled from SMTP.
-// 			// If their SMTP is valid & they are using an external email.
-// 			// We assume all this information is correct & they can send
-// 			// emails through SMTP.
-// 			if user.SMTPValid && user.ExternalEmail && user.EmailSetting != 0 {
-// 				emailBody, err := emails.GenerateEmail(r, user, schedueled[i], files)
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					hasErrors = true
-// 				}
+				log.Infof(c, "%v", files)
+				log.Infof(c, "%v", schedueled[i])
+				gmailId, gmailThreadId, err := emails.SendGmailEmail(r, user, schedueled[i], files)
+				if err != nil {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+					continue
+				}
 
-// 				emailSetting, err := controllers.GetEmailSettingById(c, r, user.EmailSetting)
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					hasErrors = true
-// 				}
+				schedueled[i].GmailId = gmailId
+				schedueled[i].GmailThreadId = gmailThreadId
 
-// 				SMTPPassword := string(user.SMTPPassword[:])
+				_, err = schedueled[i].MarkDelivered(c)
+				if err != nil {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+					continue
+				}
 
-// 				contextWithTimeout, _ := context.WithTimeout(c, time.Second*30)
-// 				client := urlfetch.Client(contextWithTimeout)
-// 				getUrl := "https://tabulae-smtp.newsai.org/send"
+				// if files are present
+				for i := 0; i < len(files); i++ {
+					files[i].Imported = true
+					files[i].Save(c)
+				}
 
-// 				sendEmailRequest := models.SMTPEmailSettings{}
-// 				sendEmailRequest.Servername = emailSetting.SMTPServer + ":" + strconv.Itoa(emailSetting.SMTPPortSSL)
-// 				sendEmailRequest.EmailUser = user.SMTPUsername
-// 				sendEmailRequest.EmailPassword = SMTPPassword
-// 				sendEmailRequest.To = schedueled[i].To
-// 				sendEmailRequest.Subject = schedueled[i].Subject
-// 				sendEmailRequest.Body = emailBody
+				// Add to emailids array
+				emailIds = append(emailIds, schedueled[i].Id)
+			}
+		} else if schedueled[i].Method == "outlook" {
+			if user.OutlookAccessToken != "" && user.Outlook {
+				err = outlook.ValidateAccessToken(r, user)
+				// Refresh access token if err is nil
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					user, err = outlook.RefreshAccessToken(r, user)
+					if err != nil {
+						hasErrors = true
+						log.Errorf(c, "%v", err)
+						continue
+					}
+				}
 
-// 				SendEmailRequest, err := json.Marshal(sendEmailRequest)
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					hasErrors = true
-// 				}
+				log.Infof(c, "%v", files)
+				log.Infof(c, "%v", schedueled[i])
+				err := emails.SendOutlookEmail(r, user, schedueled[i], files)
+				if err != nil {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+					continue
+				}
 
-// 				log.Infof(c, "%v", string(SendEmailRequest))
-// 				sendEmailQuery := bytes.NewReader(SendEmailRequest)
+				_, err = schedueled[i].MarkDelivered(c)
+				// sync.ResourceSync(r, schedueled[i].Id, "Email", "create")
+				if err != nil {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+					continue
+				}
 
-// 				req, _ := http.NewRequest("POST", getUrl, sendEmailQuery)
+				// if files are present
+				for i := 0; i < len(files); i++ {
+					files[i].Imported = true
+					files[i].Save(c)
+				}
 
-// 				resp, err := client.Do(req)
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					hasErrors = true
-// 				}
-// 				defer resp.Body.Close()
+				// Add to emailids array
+				emailIds = append(emailIds, schedueled[i].Id)
+			}
+		} else if schedueled[i].Method == "smtp" {
+			// If scheduled from SMTP.
+			// If their SMTP is valid & they are using an external email.
+			// We assume all this information is correct & they can send
+			// emails through SMTP.
+			if user.SMTPValid && user.ExternalEmail && user.EmailSetting != 0 {
+				emailBody, err := emails.GenerateEmail(r, user, schedueled[i], files)
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					hasErrors = true
+				}
 
-// 				decoder := json.NewDecoder(resp.Body)
-// 				var verifyResponse controllers.SMTPEmailResponse
-// 				err = decoder.Decode(&verifyResponse)
-// 				if err != nil {
-// 					log.Errorf(c, "%v", err)
-// 					hasErrors = true
-// 				}
+				emailSetting, err := controllers.GetEmailSettingById(c, r, user.EmailSetting)
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					hasErrors = true
+				}
 
-// 				log.Infof(c, "%v", verifyResponse)
+				SMTPPassword := string(user.SMTPPassword[:])
 
-// 				if verifyResponse.Status {
-// 					_, err := schedueled[i].MarkDelivered(c)
-// 					if err != nil {
-// 						log.Errorf(c, "%v", err)
-// 						hasErrors = true
-// 					}
-// 				} else {
-// 					log.Errorf(c, "%v", verifyResponse)
-// 					hasErrors = true
-// 				}
+				contextWithTimeout, _ := context.WithTimeout(c, time.Second*30)
+				client := urlfetch.Client(contextWithTimeout)
+				getUrl := "https://tabulae-smtp.newsai.org/send"
 
-// 				// Add to emailids array
-// 				emailIds = append(emailIds, schedueled[i].Id)
-// 			}
-// 		} else {
-// 			// If email is sent through SendGrid
-// 			// and if it doesn't have a sendGridId
-// 			// means that it hasn't been sent yet (pretty sure)
-// 			// unless there was a freak event.
-// 			if !schedueled[i].SendAt.IsZero() && schedueled[i].SendGridId == "" {
-// 				log.Infof(c, "%v", schedueled[i])
-// 				userBilling, _ := apiControllers.GetUserBilling(c, r, user)
-// 				sendGridKey := emails.GetSendGridKeyForUser(userBilling)
-// 				emailSent, emailId, err := emails.SendEmail(r, schedueled[i], user, files, sendGridKey)
-// 				if err != nil {
-// 					hasErrors = true
-// 					log.Errorf(c, "%v", err)
-// 					continue
-// 				}
+				sendEmailRequest := models.SMTPEmailSettings{}
+				sendEmailRequest.Servername = emailSetting.SMTPServer + ":" + strconv.Itoa(emailSetting.SMTPPortSSL)
+				sendEmailRequest.EmailUser = user.SMTPUsername
+				sendEmailRequest.EmailPassword = SMTPPassword
+				sendEmailRequest.To = schedueled[i].To
+				sendEmailRequest.Subject = schedueled[i].Subject
+				sendEmailRequest.Body = emailBody
 
-// 				schedueled[i].Save(c)
+				SendEmailRequest, err := json.Marshal(sendEmailRequest)
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					hasErrors = true
+				}
 
-// 				if emailSent {
-// 					// Set attachments for deletion
-// 					for i := 0; i < len(files); i++ {
-// 						files[i].Imported = true
-// 						files[i].Save(c)
-// 					}
+				log.Infof(c, "%v", string(SendEmailRequest))
+				sendEmailQuery := bytes.NewReader(SendEmailRequest)
 
-// 					_, err := schedueled[i].MarkSent(c, emailId)
-// 					if err != nil {
-// 						log.Errorf(c, "%v", err)
-// 						hasErrors = true
-// 					}
+				req, _ := http.NewRequest("POST", getUrl, sendEmailQuery)
 
-// 					_, err = schedueled[i].MarkDelivered(c)
-// 					if err != nil {
-// 						log.Errorf(c, "%v", err)
-// 						hasErrors = true
-// 					}
-// 				}
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					hasErrors = true
+				}
+				defer resp.Body.Close()
 
-// 				// Add to emailids array
-// 				emailIds = append(emailIds, schedueled[i].Id)
-// 			}
-// 		}
-// 	}
+				decoder := json.NewDecoder(resp.Body)
+				var verifyResponse controllers.SMTPEmailResponse
+				err = decoder.Decode(&verifyResponse)
+				if err != nil {
+					log.Errorf(c, "%v", err)
+					hasErrors = true
+				}
 
-// 	// Sync the emails we have delivered
-// 	sync.EmailResourceBulkSync(r, emailIds)
+				log.Infof(c, "%v", verifyResponse)
 
-// 	if hasErrors {
-// 		w.WriteHeader(500)
-// 		return
-// 	}
+				if verifyResponse.Status {
+					_, err := schedueled[i].MarkDelivered(c)
+					if err != nil {
+						log.Errorf(c, "%v", err)
+						hasErrors = true
+					}
+				} else {
+					log.Errorf(c, "%v", verifyResponse)
+					hasErrors = true
+				}
 
-// 	w.WriteHeader(200)
-// 	return
-// }
+				// Add to emailids array
+				emailIds = append(emailIds, schedueled[i].Id)
+			}
+		} else {
+			// If email is sent through SendGrid
+			// and if it doesn't have a sendGridId
+			// means that it hasn't been sent yet (pretty sure)
+			// unless there was a freak event.
+			if !schedueled[i].SendAt.IsZero() && schedueled[i].SendGridId == "" {
+				log.Infof(c, "%v", schedueled[i])
+				userBilling, _ := apiControllers.GetUserBilling(c, r, user)
+				sendGridKey := emails.GetSendGridKeyForUser(userBilling)
+				emailSent, emailId, err := emails.SendEmail(r, schedueled[i], user, files, sendGridKey)
+				if err != nil {
+					hasErrors = true
+					log.Errorf(c, "%v", err)
+					continue
+				}
+
+				schedueled[i].Save(c)
+
+				if emailSent {
+					// Set attachments for deletion
+					for i := 0; i < len(files); i++ {
+						files[i].Imported = true
+						files[i].Save(c)
+					}
+
+					_, err := schedueled[i].MarkSent(c, emailId)
+					if err != nil {
+						log.Errorf(c, "%v", err)
+						hasErrors = true
+					}
+
+					_, err = schedueled[i].MarkDelivered(c)
+					if err != nil {
+						log.Errorf(c, "%v", err)
+						hasErrors = true
+					}
+				}
+
+				// Add to emailids array
+				emailIds = append(emailIds, schedueled[i].Id)
+			}
+		}
+	}
+
+	// Sync the emails we have delivered
+	sync.EmailResourceBulkSync(r, emailIds)
+
+	if hasErrors {
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(200)
+	return
+}
